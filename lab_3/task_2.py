312968c38eafb2af8a73d30ec8e8d977ad46d18b
GolikSonya <pyfendyu@gmail.com>
2024-12-24 01:49:47 +0300

    Update task_2.py

--- lab_3/task_2.py
+++ lab_3/task_2.py
@@ -1,36 +1,20 @@
 
 
+
+
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-def find_common_participants(a, b, n=", "):
-    list_a = a.split('|')   # перечисление участников без разделителей
-    list_b = b.split('|')
+def find_common_participants(a, b, n=","):
+    list_a = a.split(n)   # перечисление участников без разделителей
+    list_b = b.split(n)
     common_participants = set(list_a).intersection(set(list_b))   # отбор общих участников среди двух групп
     list_common_participants = list(common_participants)   # преобразование множества в список для сортировки
     list_common_participants.sort()   # сортировка списка в алфавитном порядке
-    result = n.join(list_common_participants)
-    return result   # вывод отсортированного списка общих участников
+    return list_common_participants   # вывод отсортированного списка общих участников
 
-print(find_common_participants(participants_first_group, participants_second_group))   # вызов функции
-
-# TODO Провеьте работу функции с разделителем отличным от запятой
-
-print(find_common_participants(participants_first_group, participants_second_group, n="! "))
-
-
-
-def find_common_participants_(a, b, n=", "):
-    list_a = a.split('|')   # перечисление участников без разделителей
-    list_b = b.split('|')
-    common_participants = set(list_a).intersection(set(list_b))   # отбор общих участников среди двух групп
-    list_common_participants = list((common_participants))   # преобразование множества в список для сортировки
-    list_common_participants.sort()   # сортировка списка в алфавитном порядке
-    result = (n.join(list_common_participants)).split()
-    return(result)      # вывод отсортированного списка общих участников
-
-print(find_common_participants_(participants_first_group, participants_second_group))   # вызов функции
+print(find_common_participants(participants_first_group, participants_second_group, n="|"))   # вызов функции
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
 
-print(find_common_participants_(participants_first_group, participants_second_group, n="! "))
+print(find_common_participants(participants_first_group, participants_second_group))
