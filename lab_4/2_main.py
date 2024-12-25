a756640f932086f7e3ef397f4fd9b6f9d36fb002
GolikSonya <pyfendyu@gmail.com>
2024-12-25 22:06:50 +0300

    Update 2_main.py

--- lab_4/2_main.py
+++ lab_4/2_main.py
@@ -15,15 +15,16 @@ def task(a) -> None:
 
     ...  # TODO Сериализовать в файл с отступами равными 4
     import json
-    print(json.dumps(lines, indent=4))
+    with open(OUTPUT_FILENAME, 'w') as o:
+        json.dump(lines, o, indent=4)
 
 
+task(INPUT_FILENAME)
 
-task('input.csv')
-#if __name__ == '__main__':
+if __name__ == '__main__':
     # Нужно для проверки
- #   task()
+    task(INPUT_FILENAME)
 
-  #  with open(OUTPUT_FILENAME) as output_f:
-   #     for line in output_f:
-    #        print(line, end="")
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
